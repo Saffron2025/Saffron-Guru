6a237a24222987809d1e33d69d92f23c4f8c9c79
Aditya <ajha48450@gmail.com>
2025-08-25 20:42:13 +0530

    soemc hanges

--- AuraBackend/server.js
+++ AuraBackend/server.js
@@ -30,12 +30,6 @@ app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => res.send('Backend is running!'));
 
 
-app.get("/ping", (req, res) => {
-  res.send("pong ✅ Saffron Guru backend is alive");
-});
-
-
-
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
