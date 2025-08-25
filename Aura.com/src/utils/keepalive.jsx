ff545249c73896ea2305c2b86d8394080b1bea46
Aditya <ajha48450@gmail.com>
2025-08-25 22:03:13 +0530

    soemc hanges

--- Aura.com/src/utils/keepalive.jsx
+++ Aura.com/src/utils/keepalive.jsx
@@ -1,7 +1,7 @@
 // src/utils/keepAlive.js
 const keepAlive = () => {
   setInterval(() => {
-    fetch("https://saffron-guru-backend.onrender.com") 
+    fetch("https://saffron-guru-backend.onrender.com/ping") 
       .then(() => console.log("🔄 Keep-alive ping sent"))
       .catch((err) => console.error("❌ Keep alive error:", err));
   }, 10 * 60 * 1000); // every 10 min
